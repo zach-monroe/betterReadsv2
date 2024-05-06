8b4a95fdf36fb9509215d2799461aba4833cdc0a
Zach Monroe <z@localhost.localdomain>
2024-05-05 23:09:11 -0400

    Tailwindcss is now configured properly.

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../output.css";
 const NavBar = () => {
   return (
     <div className="container">
-      <li className="text-blue">
+      <li className="text-blue text-xl">
         <Link className="mx-auto text-blue" to="/">
           Home
         </Link>
