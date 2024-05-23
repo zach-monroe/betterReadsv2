04f10b87fc6791b6770178b98993e3c121445a7f
Zach Monroe <z@localhost.localdomain>
2024-05-23 15:54:27 -0400

    Added Modal for confirming deletion

--- client/src/components/Book/DeleteButton.jsx
+++ client/src/components/Book/DeleteButton.jsx
@@ -1,11 +1,33 @@
 import React, { useState } from "react";
 import "../../output.css";
 import { useAuth } from "../../AuthProvider";
+import Modal from "react-modal/lib/components/Modal";
 
 function DeleteButton(props) {
   const [isDeleting, setIsDeleting] = useState(false);
+  const [modalIsOpen, setIsOpen] = useState(false);
   const { user } = useAuth();
 
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      backgroundColor: "#242424",
+    },
+  };
+
+  const openModal = (e) => {
+    e.preventDefault();
+    setIsOpen(true);
+  };
+
+  function closeModal() {
+    setIsOpen(false);
+  }
   const handleDelete = async (e) => {
     e.preventDefault();
     setIsDeleting(true); // Set deleting state to true
@@ -28,21 +50,51 @@ function DeleteButton(props) {
     } catch (error) {
       console.error("Error deleting item:", error);
     } finally {
+      setIsOpen(false);
       setIsDeleting(false);
     }
   };
 
   return (
-    <form onSubmit={handleDelete}>
-      <input type="hidden" name="id" value={props.id} />
-      <button
-        className="rounded-full bg-red-800 px-4 mt-4"
-        type="submit"
-        disabled={isDeleting}
+    <div>
+      <form onSubmit={openModal}>
+        <input type="hidden" name="id" value={props.id} />
+        <button
+          className="rounded-full bg-red-800 px-4 mt-4"
+          type="submit"
+          disabled={isDeleting}
+        >
+          {isDeleting ? "Deleting..." : "Delete"}
+        </button>
+      </form>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestclose={closeModal}
+        style={customStyles}
       >
-        {isDeleting ? "Deleting..." : "Delete"}
-      </button>
-    </form>
+        <div className="flex justify-center items-center bg-material text-primary">
+          <div>
+            <div className="flex justify-center text-center py-4">
+              <p>Are You Sure you want to delete this post?</p>
+            </div>
+            <div className="flex justify-around py-4">
+              <button
+                className="rounded-full px-4 bg-material border-primary text-primary border-2"
+                onClick={closeModal}
+              >
+                Close
+              </button>
+              <button
+                className="rounded-full px-4 bg-red-800 border-primary text-primary border-2"
+                onClick={handleDelete}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </Modal>
+    </div>
   );
 }
 
