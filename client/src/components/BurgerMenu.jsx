6740107ce3296487914f2f0fe7e0a3e8a841374c
Zach Monroe <z@localhost.localdomain>
2024-05-29 21:44:50 -0400

    Segmented the LogOut Modal into a separate component

--- client/src/components/BurgerMenu.jsx
+++ client/src/components/BurgerMenu.jsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 import { slide as Menu } from "react-burger-menu";
 import { Link } from "react-router-dom";
 import { useAuth } from "../AuthProvider";
-import Modal from "react-modal/lib/components/Modal";
+import LogOutModal from "./LogOutModal";
+import Modal from "react-modal";
 
 Modal.setAppElement("#root");
 
@@ -22,18 +23,6 @@ function Burger() {
 
     document.dispatchEvent(event);
   }
-
-  const customStyles = {
-    content: {
-      top: "50%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-50%",
-      transform: "translate(-50%, -50%)",
-      backgroundColor: "#242424",
-    },
-  };
 
   function openModal() {
     simulateEscapeKeyPress();
@@ -70,29 +59,7 @@ function Burger() {
         )}
         {token ? <button onClick={openModal}>Log-Out</button> : null}
       </Menu>
-      <Modal isOpen={isOpen} onRequestClose={closeModal} style={customStyles}>
-        <div className="flex justify-center items-center bg-material text-primary">
-          <div>
-            <div className="flex justify-center text-center py-4">
-              <p>Are you sure you want to Log-Out?</p>
-            </div>
-            <div className="flex justify-around py-4">
-              <button
-                className="rounded-full px-4 bg-material border-primary text-primary border-2"
-                onClick={closeModal}
-              >
-                Close
-              </button>
-              <button
-                className="rounded-full px-4 bg-red-800 border-primary text-primary border-2"
-                onClick={logOut}
-              >
-                Log-Out
-              </button>
-            </div>
-          </div>
-        </div>
-      </Modal>
+      <LogOutModal isOpen={isOpen} closeModal={closeModal} logOut={logOut} />
     </div>
   );
 }
